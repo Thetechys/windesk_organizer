9cee123d6d42436e3bf33d776534e7c1551336a6
TheMechanicYS <96005936+Thetechys@users.noreply.github.com>
2023-11-25 21:33:35 +0800

    Update windesk_organizer.py
    Clean up code and added some comment.

--- windesk_organizer.py
+++ windesk_organizer.py
@@ -4,42 +4,37 @@ from tkinter import messagebox
 import shutil
 
 
-
-
+#get user home path
 org_path = os.path.expanduser('~' + '\\Desktop\\')
-
-typelookup = ['png', 'jpg', 'jpeg', 'pdf']
+typelookup = ['png', 'jpg', 'jpeg', 'pdf'] #append all types(extension) that needed to be organized
 
 
 class organizer:
-
 
     def __init__(self) -> None:
         
         self.timestamp = self.get_date()
         self.items = None
-        self.file_detect()
+        self.file_detect() #fill up 'self.items' when instance initialed
 
 
     
     def get_date(self):
+        
         now = datetime.now().strftime('%d%m%Y_%H%M')
         return now
     
 
+    
     def file_detect(self):
 
         files = os.listdir(org_path)
         detected = []
 
         for file in files:
-
             for type in typelookup:
                 if file.endswith(type):
                     detected.append(file)
-                
-        print(detected)
-
         #if there's file matches lookup
         #assign them to instance variable 'self.items'
         if len(detected) > 0:
@@ -50,12 +45,11 @@ class organizer:
     def create_folder(self):
 
         if self.items is not None:
-
             #create a folder that named with timestamp
             foldername = 'organize_' + self.timestamp
             create_path = os.path.join(org_path, foldername)
             os.mkdir(create_path)
-
+            
             for item in self.items:
                 shutil.move(org_path + item, create_path)
 
@@ -70,9 +64,7 @@ class organizer:
 
 
 
-
 if __name__ == '__main__':
-
 
     inst1 = organizer()
     inst1.main()
